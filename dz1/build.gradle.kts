e00c670acd39c8de9c1bac7e5b92182766595e98
Петров Илья Олегович <ip217s24@csc.sibsutis.ru>
2025-02-12 11:25:50 +0700

    add tests

--- dz1/build.gradle.kts
+++ dz1/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.10" // замените на нужную версию Kotlin
+    kotlin("jvm") version "1.8.10"
     application
 }
 
@@ -10,16 +10,15 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    // JUnit 5
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testRuntimeOnly ("org.junit.platform:junit-platform-launcher:1.8.2")
 }
 
 tasks.test {
-    useJUnitPlatform() // Указывает Gradle использовать платформу JUnit
+    useJUnitPlatform()
 }
 
 application {
-    mainClass.set("src/main/java/org/example/FileFilter.java")  // Замените на путь к вашему основному классу
+    mainClass.set("src/main/java/org/example/FileFilter.java")
 }
--- dz1/src/test/java/org.example/FileFilterTest.java
+++ dz1/src/test/java/org.example/FileFilterTest.java
@@ -1,4 +1,47 @@
 package org.example;
 
-public class FileFilterTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
+class FileFilterTest {
+
+    @TempDir
+    Path tempDir;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        System.out.println("Временная директория: " + tempDir);
+
+        Path inputFile = tempDir.resolve("input.txt");
+        Files.write(inputFile, List.of("123", "45.67", "test", "test1", "42"));
+    }
+
+    @Test
+    void testFileFilter() throws IOException {
+        System.out.println("Тестируем в директории: " + tempDir);
+
+        Path inputFile = tempDir.resolve("input.txt");
+
+        FileFilter.main(new String[]{inputFile.toString(), "--output", tempDir.toString(), "--prefix", "output_"});
+
+        System.out.println("Файлы в tempDir: " + Files.list(tempDir).map(Path::getFileName).toList());
+
+        Path intFile = tempDir.resolve("output_integers.txt");
+        Path floatFile = tempDir.resolve("output_floats.txt");
+        Path strFile = tempDir.resolve("output_strings.txt");
+
+        assertTrue(Files.exists(intFile), "output_integers.txt не создан");
+        assertTrue(Files.exists(floatFile), "output_floats.txt не создан");
+        assertTrue(Files.exists(strFile), "output_strings.txt не создан");
+
+        assertEquals(List.of("123", "42"), Files.readAllLines(intFile));
+        assertEquals(List.of("45.67"), Files.readAllLines(floatFile));
+        assertEquals(List.of("test", "test1"), Files.readAllLines(strFile));
+    }
 }
