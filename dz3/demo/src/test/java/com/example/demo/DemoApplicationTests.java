b92b5f8b33b1717db50509ebd1bbca86e6a78d44
Петров Илья Олегович <ip217s24@csc.sibsutis.ru>
2025-03-04 23:44:33 +0700

    add tests for dz3

--- dz3/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ dz3/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
-class DemoApplicationTests {
+@AutoConfigureMockMvc
+public class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void testAuthorPage() throws Exception {
+		mockMvc.perform(get("/author"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("author-info"));
 	}
-
 }
